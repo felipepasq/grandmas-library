2adc363617f134f8cdd75380a55c605915395bb0
Felipe Pasqua <felipe.rodrigues@credihome.com.br>
2022-02-10 23:07:42 -0300

    FEAT: Added shelfs and book

--- src/Components/Bookshelf/Bookshelf.jsx
+++ src/Components/Bookshelf/Bookshelf.jsx
@@ -1,22 +1,76 @@
-import styled from "styled-components"
+    import styled from "styled-components"
 
 
-const BookShelfContainer = styled.div`
-background-image: url(/assets/bookcase.svg);
-transform: translateY(-160px);
-background-size: 400px;
-display: flex;
-height: 245px;
-width: 400px;
-z-index: 1;
-margin-left: 70px;
-`
+    const BookShelfContainer = styled.div`
+        background-image: url(/assets/bookcase.svg);
+        transform: translateY(-160px);
+        background-size: 400px;
+        display: flex;
+        flex-direction: column;
+        height: 245px;
+        width: 400px;
+        z-index: 1;
+        margin-left: 70px;
+        padding-left: 20px;
+    `
 
+    const TopShelf = styled.div`
+        width: 70%;
+        height: 75px;
+        position: absolute;
+        top: 50px;
+        display: inline-flex;
+        padding: 0 2px 0;
+    `
 
-export function BookShelf () {
-    return(
-        <BookShelfContainer>
+    const BottomShelf = styled.div`  
+        width: 70%;
+        height: 75px;
+        position: absolute;
+        top: 150px;
+        display: inline-flex;
+        justify-content: flex-end;
+        padding: 0 2px 0;
+    `
+    const Book = styled.div`
+        height: fit-content;
+        width: fit-content;
+     
         
-        </BookShelfContainer>
-    )
-}
+        img{
+            
+            height: 75px;
+            width: 26.666px;
+            object-fit: fill;
+        }
+
+    `
+
+
+    export function BookShelf () {
+        return(
+            <BookShelfContainer>
+            <TopShelf>
+                <Book>
+                    <img src="/assets/book_a.svg" alt="" />
+                    <img src="/assets/book_b.svg" alt="" />
+                    <img src="/assets/book_c.svg" alt="" />
+                    <img src="/assets/book_d.svg" alt="" />
+                    <img src="/assets/book_e.svg" alt="" />
+                    <img src="/assets/book_f.svg" alt="" />
+                    <img src="/assets/book_g.svg" alt="" />
+                    <img src="/assets/book_h.svg" alt="" />
+                    <img src="/assets/book_i.svg" alt="" />
+
+                </Book>
+             
+             
+            </TopShelf>
+            <BottomShelf>
+                <Book>
+                    <img src="/assets/book_a.svg" alt="" />
+                </Book>
+            </BottomShelf>
+            </BookShelfContainer>
+        )
+    }
