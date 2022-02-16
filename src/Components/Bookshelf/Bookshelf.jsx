d835802e14e1d085a18916c01b5146039dfded0a
Felipe Pasqua <felipe.rodrigues@credihome.com.br>
2022-02-15 23:41:28 -0300

    Revert "DEV: Fixing css responsiveness"
    This reverts commit ae15ee6a6b74e2406f6c86fa9c88126e254d8886.

--- src/Components/Bookshelf/Bookshelf.jsx
+++ src/Components/Bookshelf/Bookshelf.jsx
@@ -5,9 +5,8 @@ import devices from "../../utils/devices";
 
 
 
-const BookShelfContainer = styled.div`
+    const BookShelfContainer = styled.div`
         background-image: url(/assets/bookcase.svg);
-        background-repeat: no-repeat;
         margin-top: -155px;
         background-size: 400px;
         display: flex;
@@ -20,26 +19,26 @@ const BookShelfContainer = styled.div`
 
 
         @media ${devices.tablet}{
-        height: 308px;
-        width: 500px;
-        background-size: 500px;
-        margin-top: -203px;
+        height: 338px;
+        width: 550px;
+        background-size: 550px;
+        margin-top: -220px;
         padding-left: 30px;
         }
     `
 
-const Shelf = styled.div`
+    const Shelf = styled.div`
         width: fit-content;
         height: 75px;
-        margin-top: ${props => props.bottomShelf ? '22px' : '50px'};
+        margin-top: ${props=>props.bottomShelf?'22px':'50px'};
       
         display: inline-flex;
-        justify-content: ${props => props.bottomShelf && 'flex-end'};
+        justify-content: ${props=>props.bottomShelf && 'flex-end'};
         padding: 0 2px 0;
         min-width: 278px;
 
         @media ${devices.tablet}{
-           margin-top: ${props => props.bottomShelf ? '31px' : '67px'};
+           margin-top: ${props=>props.bottomShelf?'31px':'67px'};
            width: 380px;
            height: 105px;
         }
@@ -47,8 +46,8 @@ const Shelf = styled.div`
 
     `
 
-
-const Book = styled.div`
+ 
+    const Book = styled.div`
      
         img{   
             height: 75px;
@@ -63,111 +62,111 @@ const Book = styled.div`
 
     `
 
+    
 
+    export function BookShelf () {
 
-export function BookShelf() {
+        
 
-
-
-    const {
+       const {
         topBooks,
         bottomBooks,
         handleOnDragEnd,
-    } = useBook();
+       } = useBook();
+       
 
+      
+       
+      
 
-
-
-
-
-
-
-
-    return (
-        <BookShelfContainer>
-
+  
+     
+        
+        return  (
+            <BookShelfContainer>
+               
             <DragDropContext onDragEnd={handleOnDragEnd}>
                 <Droppable droppableId="topShelf" direction="horizontal">
-                    {(provided) => {
-                        return (
-                            <Shelf
-                                {...provided.droppableProps}
-                                ref={provided.innerRef}
-                                style={{
-                                    display: "inline-flex"
-                                }}
+                {(provided) => {
+                    return  (
+                        <Shelf
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                        style={{
+                            display:"inline-flex"
+                        }}
+                        >
+                       {topBooks.map((value,index) => {
+                           
+                            return ( 
+                            <Draggable
+                            key={value.title}
+                            draggableId={value.title}
+                            index={index}
                             >
-                                {topBooks.map((value, index) => {
-
+                                {(provided) => {
                                     return (
-                                        <Draggable
-                                            key={value.title}
-                                            draggableId={value.title}
-                                            index={index}
-                                        >
-                                            {(provided) => {
-                                                return (
-                                                    <Book
-                                                        ref={provided.innerRef}
-                                                        {...provided.draggableProps}
-                                                        {...provided.dragHandleProps}
-                                                    >
-                                                        <img src={value.url} alt={value.title} />
-                                                    </Book>
-
-                                                )
-                                            }}
-
-                                        </Draggable>
-                                    );
-                                })}
-                                {provided.placeholder}
-                            </Shelf>
-                        )
-                    }}
-                </Droppable>
-
+                                <Book
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                >
+                                    <img src={value.url} alt={value.title}/>
+                                </Book>
+                            
+                                    )
+                                }}
+                                
+                            </Draggable>
+                            );
+                        })}
+                         {provided.placeholder}
+                        </Shelf>
+                    )
+                }}
+                </Droppable>   
+                
                 <Droppable droppableId="bottomShelf" direction="horizontal">
-                    {(provided) => {
-                        return (
-                            <Shelf bottomShelf
-                                {...provided.droppableProps}
-                                ref={provided.innerRef}
-                                style={{
-                                    display: "inline-flex"
-                                }}
+                {(provided) => {
+                    return  (
+                        <Shelf bottomShelf
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                        style={{
+                            display:"inline-flex"
+                        }}
+                        >
+                       {bottomBooks.map((value,index) => {
+                            return (
+                            <Draggable
+                            key={value.title}
+                            draggableId={value.title}
+                            index={index}
                             >
-                                {bottomBooks.map((value, index) => {
+                                {(provided) => {
                                     return (
-                                        <Draggable
-                                            key={value.title}
-                                            draggableId={value.title}
-                                            index={index}
-                                        >
-                                            {(provided) => {
-                                                return (
-                                                    <Book
-                                                        ref={provided.innerRef}
-                                                        {...provided.draggableProps}
-                                                        {...provided.dragHandleProps}
-                                                    >
-                                                        <img src={value.url} alt={value.title} />
-                                                    </Book>
-
-                                                )
-                                            }}
-
-                                        </Draggable>
-                                    );
-                                })}
-                                {provided.placeholder}
-                            </Shelf>
-                        )
-                    }}
-                </Droppable>
-
+                                <Book
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                >
+                                    <img src={value.url} alt={value.title}/>
+                                </Book>
+                            
+                                    )
+                                }}
+                                
+                            </Draggable>
+                            );
+                        })}
+                         {provided.placeholder}
+                        </Shelf>
+                    )
+                }}
+                </Droppable>     
+        
             </DragDropContext>
-
-        </BookShelfContainer>
-    )
-}
+           
+            </BookShelfContainer>
+        )
+    }
